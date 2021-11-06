29969e1d930dfb87e1945cb6057d55581c567341
Cadenza <mirandalucsb@gmail.com>
2021-11-05 18:59:19 -0700

    experimented with dragging and dropping javascript and flexbox order

---
+++ assets/js/matrix.js
@@ -0,0 +1,27 @@
+// var C = document.getElementById("C");
+// C.onmousedown = down;
+
+// function down(e){
+//     C.style.position = "absolute";
+//     e.preventDefault();
+//     var mouseX = e.clientX;
+//     var mouseY = e.clientY;
+//     C.style.top = mouseY + "px";
+//     C.style.left= mouseX + "px";
+//     document.onmousemove = drag;
+//     document.onmouseup = stopMoving;
+// }
+
+// function drag(e){
+//     e.preventDefault();
+//     var mouseX = e.clientX;
+//     var mouseY = e.clientY;
+//     C.style.top = mouseY -10 + "px";
+//     C.style.left= mouseX - 10 + "px";
+// }
+
+// function stopMoving(){
+//     document.onmousemove = null;
+//     C.style.position = "static";
+// }
+
